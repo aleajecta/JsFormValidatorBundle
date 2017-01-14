de52845b845492f75b474941b5c88499a5dbccaa
Sébastien Hawkins <greenseed@aleajecta.com>
2017-01-14 16:35:57 -0500

    xml to yml

--- DependencyInjection/FpJsFormValidatorExtension.php
+++ DependencyInjection/FpJsFormValidatorExtension.php
@@ -26,7 +26,7 @@ class FpJsFormValidatorExtension extends Extension
 
         $container->setParameter($this->getAlias() . '.config', $config);
 
-        $loader = new Loader\YmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
     }
 
